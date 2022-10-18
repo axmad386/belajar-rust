22450f3cf2c987620581cb0eeb105400e7da1443
Akhmad Salafudin <axmad386@gmail.com>
2022-10-18 17:06:53 +0700

    feat: module system

---
+++ src/enums.rs
@@ -0,0 +1,24 @@
+
+pub fn run(){
+    let first_letter = "Halo Indonesia";
+    let second_letter = "hallo";
+    let longest = longest(first_letter, second_letter);
+
+    match longest {
+        StrOrInt::Int(value) => println!("integer {}", value),
+        StrOrInt::Str(value) => println!("string {}", value),
+    }
+}
+
+enum StrOrInt<'a> {
+    Str(&'a str),
+    Int(i32),
+}
+
+fn longest<'a>(first: &'a str, second: &'a str) -> StrOrInt<'a> {
+    if first.len() > second.len() {
+        StrOrInt::Str(first)
+    } else {
+        StrOrInt::Int(0)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
+pub mod enums;
+pub mod vecs;
+
 fn main() {
-    let first_letter = "Halo Indonesia";
-    let second_letter = "hallo";
-    let longest = longest(first_letter, second_letter);
-
-    match longest {
-        StrOrInt::Int(value) => println!("integer {}", value),
-        StrOrInt::Str(value) => println!("string {}", value),
-    }
-}
-
-enum StrOrInt<'a> {
-    Str(&'a str),
-    Int(i32),
-}
-
-fn longest<'a>(first: &'a str, second: &'a str) -> StrOrInt<'a> {
-    if first.len() > second.len() {
-        StrOrInt::Str(first)
-    } else {
-        StrOrInt::Int(0)
-    }
+    enums::run();
+    vecs::run();
 }
