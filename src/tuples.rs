4994a9146e2cc0ed2eb4e9796aba129cf4bde849
Akhmad Salafudin <axmad386@gmail.com>
2022-10-19 17:04:13 +0700

    feat: implement custom fmt::Display untuk tupple

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,3 +1,6 @@
+use core::fmt;
+
+
 pub fn run() {
     println!("===tuples===");
     fn reverse_pair(pair: (i32, bool)) -> (bool, i32) {
@@ -13,6 +16,16 @@ pub fn run() {
 
     #[derive(Debug)]
     struct Matrix (f32, f32, f32, f32);
+
+    // secara default custom struct tidak bisa diprint menggunakan default fmt::Display
+    // jadi kita harus mendefinisikan sendiri cara rendernya
+    impl fmt::Display for Matrix {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "[{} {}]\n[{} {}]", self.0, self.1, self.2, self.3)
+        }
+    }
+
     let matrix = Matrix(1.1, 1.2, 1.3, 1.4);
-    println!("{:?}", matrix);
+    println!("{:?}", matrix); // ini akan diprint secara default menggunakan fmt::Debug
+    println!("{}", matrix); // ini akan dirender sesuai yang kita declare di atas oleh fmt::Display
 }
